4d9c3230c94182cee30993c5fa39ec74392d0239
Coda Hale <coda.hale@gmail.com>
2021-05-17 17:24:54 -0600

    Fix benchmarks.

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,16 +1,18 @@
 extern crate criterion;
 extern crate nanostat;
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
+
+use nanostat::Summary;
 
 fn summarize(c: &mut Criterion) {
     let v = vec![0.0; 1000];
-    c.bench_function("summarize", move |b| b.iter(|| nanostat::Summary::of(&v)));
+    c.bench_function("summarize", move |b| b.iter(|| v.iter().collect::<Summary>()));
 }
 
 fn compare(c: &mut Criterion) {
-    let s1 = nanostat::Summary::of(&vec![0.0; 10]);
-    let s2 = nanostat::Summary::of(&vec![0.1; 10]);
+    let s1: Summary = vec![0.0; 10].iter().collect();
+    let s2: Summary = vec![0.1; 10].iter().collect();
 
     c.bench_function("compare", move |b| {
         b.iter(|| s1.compare(&s2, nanostat::Confidence::P98))
